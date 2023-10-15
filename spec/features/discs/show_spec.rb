e48e328de04c007b3533f117f7ce38b217589b7c
Kam Kennedy <kameronk013@gmail.com>
2023-10-15 10:34:21 -0600

    add disc show spec

--- spec/features/discs/show_spec.rb
+++ spec/features/discs/show_spec.rb
@@ -1,22 +1,20 @@
 require "rails_helper"
 
 RSpec.describe 'the discs show page' do
-  describe '#initialize' do 
-  #   [x] done
+  it 'displays the disc attributes' do
+    innova = Manufacturer.create(name: "Innova", is_american: true, num_pros: 15)
+    disc = Disc.create(manufacturer_id:innova.id, mold_name:"Destroyer", overstable:true, speed:12)
+    #   [x] done
 
-  # User Story 4, Child Show 
+    # User Story 4, Child Show 
 
-  # As a visitor
-  # When I visit '/child_table_name/:id'
-  # Then I see the child with that id including the child's attributes
-  # (data from each column that is on the child table)
-  it 'displays the disc mold' do
-      disc = Disc.create(manufacturer_id:1, mold_name:"Destroyer", overstable:true, speed:12)
-      visit "/discs/#{disc.id}"
-
-  end
-
-  it 'displays the disc speed' 
-
+    # As a visitor
+    # When I visit '/child_table_name/:id'
+    visit "/discs/#{disc.id}"
+    # Then I see the child with that id including the child's attributes
+    expect(page).to have_content(disc.mold_name)
+    expect(page).to have_content(disc.overstable)
+    expect(page).to have_content(disc.speed)
+    # (data from each column that is on the child table)
   end
 end
